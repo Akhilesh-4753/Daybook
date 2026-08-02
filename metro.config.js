ca9504729a0e4da30a6100aec696a2ff867ee7e4
Akhilesh <akhipn4753@gmail.com>
2026-08-03 00:30:02 +0530

    diagnosed and resolved the issue causing the EAS Android Gradle build failure.

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,3 @@
-try {
-  require('./scripts/recolor.js');
-  require('./scripts/update_launcher_icons.js');
-} catch (e) {
-  console.warn('Recolor script notice:', e.message);
-}
-
 const { getDefaultConfig } = require('expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
